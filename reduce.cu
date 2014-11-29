33da6c2d010487ad26154582af45434a8da9befe
root <root@ip-172-31-2-118.ec2.internal>
2014-11-28 21:59:55 -0800

    Added Struct generation

--- reduce.cu
+++ reduce.cu
@@ -1,27 +1,39 @@
 #include "stdio.h"
 
-void generateData(int a,int **b); 
+void generateData(int a,float **b); 
+
+typedef struct {
+  int  id;
+  int  binId1;
+  int  binId2;
+  float *price;
+    } Security; 
+
+void generateData(int totalCount,float **ptr) {
+  *ptr = NULL; 
+  *ptr = (float *) malloc(totalCount * sizeof(float));
+  for(int i = 0;i < totalCount; i++) {
+    *((*ptr) + i) = 20.0+((double)rand()/(double)(RAND_MAX-1))*100;
+  }
+}
+
+void generateSecurityData(int perBin,int bins,Security **securities) {
+  int total = 1;
+  *securities = NULL;
+  *securities = (Security *)malloc(perBin*bins*sizeof(Security));
+  for(int i =0;i < bins; i++) {
+    for(int q = 0; q < perBin; q++) {
+      Security sec;
+      sec.id = total+1;
+      sec.binId1 = (i+1); 
+      generateData(2048,&sec.price);
+      *((*securities + total)) = sec;
+    }
+  }
+}   
 
 int main() { 
-  int count = 100; 
-  int* elements; 
-  generateData(count,&elements); 
-  int sum = 0;
-  for(int i = 0;i < count;i++) {
-    sum = sum + elements[i];
-  }
-  printf("Sum %d\n",sum); 
-  free(elements);
+  Security *securities;
+  generateSecurityData(256,32,&securities);
   return 0;
 } 
-
-
-void generateData(int totalCount,int **ptr) {
-  *ptr = NULL; 
-  *ptr = (int *) malloc(totalCount * sizeof(int));
-  for(int i = 0;i < totalCount; i++) {
-    *((*ptr) + i) = i;
-  }
-  printf("Data generated\n");
-}
-    
