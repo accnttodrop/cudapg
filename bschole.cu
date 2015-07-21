ab877462c51f6b3af359b54b01bfc5727ac45229
lnxusr <lnxusr@laptopusr-GE72-2QF.home>
2015-07-21 00:23:46 -0400

    Tes

--- bschole.cu
+++ bschole.cu
@@ -2,7 +2,7 @@
 
 #define N 256
 #define THREADS_PER_BLOCK 256
-
+//Test
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
